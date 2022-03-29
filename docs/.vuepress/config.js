5ec4adc2f8e438a85e3a5b34942350bcf8fc57a0
taogongzhao <364645419@qq.com>
2022-03-29 16:46:47 +0800

    移库添加操作人

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,56 @@
 module.exports = {
-  title: 'Chen\'s blog',
-  description: '我的个人网站',
+  title: '个人主页',
+  description: 'Personal Website',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/images/photo.jpg' }],
+    ['link', { rel: 'manifest', href: '/images/photo.jpg' }],
+    ['link', { rel: 'apple-touch-icon', href: '/images/photo.jpg' }],
+    ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache'}],
+    ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache,must-revalidate'}],
+    ['meta', { 'http-quiv': 'expires', cotent: '0'}]
   ],
-  base: '/', // 这是部署到github相关的配置
+  serviceWorker: true, // 是否开启 PWA
+  base: '/', // 部署到github相关的配置
   markdown: {
-    lineNumbers: false // 代码块显示行号
+    lineNumbers: true // 代码块是否显示行号
   },
   themeConfig: {
     nav:[ // 导航栏配置
       {text: '前端基础', link: '/accumulate/' },
       {text: '算法题库', link: '/algorithm/'},
+      {text: '诗和远方', link: '/others/'},
       {text: '微博', link: 'https://baidu.com'}      
     ],
+    // sidebar:{
+    //   '/accumulate/': [
+    //       {
+    //         title: '前端积累',
+    //         children: [
+    //           '/accumulate/1.html',
+    //           '/accumulate/2.html',
+    //           '/accumulate/3.html',
+    //           '/accumulate/4.html',
+    //           '/accumulate/5.html',
+    //           '/accumulate/6.html',
+    //           '/accumulate/7.html',
+    //           '/accumulate/8.html',
+    //           '/accumulate/9.html',
+    //           '/accumulate/10.html',
+    //           '/accumulate/11.html',
+    //         ]
+    //       }
+    //     ],
+    //     '/algorithm/': [
+    //       '/algorithm/', 
+    //       {
+    //         title: '第二组侧边栏下拉框的标题1',
+    //         children: [
+    //           '/algorithm/' 
+    //         ]
+    //       }
+    //     ]
+    // },
     sidebar: 'auto', // 侧边栏配置
-    sidebarDepth: 2, // 侧边栏显示2级
+    sidebarDepth: 2
   }
 };
